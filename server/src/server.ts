be70eed4160e3206e64f54210cf1b8c21ae97dd0
viniciuspadovam <padovam3373@gmail.com>
2022-09-15 03:31:25 -0300

    primeiro commit

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3333;
+
+app.get("/", (req, res) => {
+    return res.json([
+        { id: 1, name: "Anuncio 1" },
+        { id: 2, name: "Anuncio 2" },
+        { id: 3, name: "Anuncio 3" },
+    ]);
+});
+
+app.listen(port);
