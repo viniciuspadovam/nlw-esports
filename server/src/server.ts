285bc35a571b868cef3c44eba95c86552e94f084
viniciuspadovam <padovam3373@gmail.com>
2022-09-15 03:59:50 -0300

    criação das rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,26 @@ import express from 'express';
 const app = express();
 const port = 3333;
 
-app.get("/", (req, res) => {
+/**
+ * Route to get a list os games.
+ */
+app.get("/games", (req, res) => {
+    return res.json([]);
+});
+
+/**
+ * Route to create a new ad.
+ */
+app.post("/ads", (req, res) => {
+    return res.json([]);
+});
+
+/**
+ * Route to get ad by an game id.
+ */
+app.get("/games/:id/ads", (req, res) => {
+    //const gameId = req.params.id;
+
     return res.json([
         { id: 1, name: "Anuncio 1" },
         { id: 2, name: "Anuncio 2" },
@@ -11,4 +30,15 @@ app.get("/", (req, res) => {
     ]);
 });
 
-app.listen(port);
+ /**
+  * Route to get discord by an ad id.
+  */
+app.get("/ads/:id/discord", (req, res) => {
+    //const adId = req.params.id;
+
+    return res.json([]);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}/ads`);
+});
